1c55680ca5c22ac732e96e9fa48054d9dc576c24
awsengineer <mehdi@awsengineer.com>
2016-06-09 12:39:51 +1000

    Update mycloudinit.sh

--- mycloudinit.sh
+++ mycloudinit.sh
@@ -6,7 +6,7 @@ yum install httpd python34 python34-pip -y
 /usr/bin/pip-3.4 install PyMySQL
 chkconfig httpd on
 service httpd start
-wget https://raw.githubusercontent.com/ezzobad/ted/master/ted.py -O /var/www/cgi-bin/ted
-wget https://raw.githubusercontent.com/ezzobad/ted/master/ted.py -O /var/www/cgi-bin/insert.py
+wget https://raw.githubusercontent.com/awsengineer/MyTED/master/ted.py -O /var/www/cgi-bin/ted
+wget https://raw.githubusercontent.com/awsengineer/MyTED/master/insert.py -O /var/www/cgi-bin/insert.py
 chmod +x /var/www/cgi-bin/ted
 chmod +x /var/www/cgi-bin/insert.py
