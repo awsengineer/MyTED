c4824d93c46c2d0ffc86275784bcf1b78e899abc
awsengineer <mehdi@awsengineer.com>
2016-06-09 12:55:11 +1000

    Update ted.py

--- ted.py
+++ ted.py
@@ -7,9 +7,9 @@ import re
 import pymysql.cursors
 
 # Connect to the database
-db = pymysql.connect(host='localhost',
-                     user='root',
-                     db='mehdidb',
+db = pymysql.connect(host='52.201.229.228',
+                     user='salehim',
+                     password='abcABC123',
                      charset='utf8mb4',
                      cursorclass=pymysql.cursors.DictCursor)
 
